9dd189935b7e4e0516b29683552098d1a79cff4c
OchirnyamB <ochirnyam@gmail.com>
2025-10-15 00:34:13 -0400

    Add code for sign in

--- airhotel/src/main/java/com/project/airhotel/AirhotelApplication.java
+++ airhotel/src/main/java/com/project/airhotel/AirhotelApplication.java
@@ -2,8 +2,10 @@ package com.project.airhotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class AirhotelApplication {
 
 	public static void main(String[] args) {
--- airhotel/src/main/java/com/project/airhotel/controller/RouteController.java
+++ airhotel/src/main/java/com/project/airhotel/controller/RouteController.java
@@ -1,13 +1,32 @@
 package com.project.airhotel.controller;
 
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class RouteController {
 
   @GetMapping({"/", "/index"})
   public String index() {
     return "Welcome to our hotel reservation App AirHotel!";
+  }
+
+  @GetMapping("/login")
+  public String login() {
+    return "custom-login"; // resolves to src/main/resources/templates/custom-login.html via Thymeleaf
+  }
+
+  // sample protected endpoint
+  @GetMapping("/profile")
+  @ResponseBody
+  public String restricted(OAuth2AuthenticationToken token, Model model) {
+    return String.format(
+        "Successfully logged in as %s | Name: %s",
+        token.getPrincipal().getAttribute("email"),
+        token.getPrincipal().getAttribute("name")
+    );
   }
 }
