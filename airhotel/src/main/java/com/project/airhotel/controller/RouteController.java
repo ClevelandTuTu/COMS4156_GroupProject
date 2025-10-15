572af364a7b2b4d87841fa1182366ff9d9ca36fa
OchirnyamB <ochirnyam@gmail.com>
2025-10-15 01:08:42 -0400

    Add code for logout

--- airhotel/src/main/java/com/project/airhotel/controller/RouteController.java
+++ airhotel/src/main/java/com/project/airhotel/controller/RouteController.java
@@ -5,11 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class RouteController {
 
   @GetMapping({"/", "/index"})
+  @ResponseBody
   public String index() {
     return "Welcome to our hotel reservation App AirHotel!";
   }
@@ -19,14 +21,17 @@ public class RouteController {
     return "custom-login"; // resolves to src/main/resources/templates/custom-login.html via Thymeleaf
   }
 
+  @GetMapping("/logout-success")
+  @ResponseBody
+  public String logoutSuccess() {
+    return "You've been logged out successfully!"; // resolves to src/main/resources/templates/custom-login.html via Thymeleaf
+  }
+
   // sample protected endpoint
   @GetMapping("/profile")
-  @ResponseBody
   public String restricted(OAuth2AuthenticationToken token, Model model) {
-    return String.format(
-        "Successfully logged in as %s | Name: %s",
-        token.getPrincipal().getAttribute("email"),
-        token.getPrincipal().getAttribute("name")
-    );
+    model.addAttribute("email", token.getPrincipal().getAttribute("email"));
+    model.addAttribute("name", token.getPrincipal().getAttribute("name"));
+    return "profile";
   }
 }
